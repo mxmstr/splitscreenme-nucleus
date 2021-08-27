a3ee037055ca05f4acd2ed15e814360aa688cf6a
Ilyaki <ilyaki@protonmail.com>
2021-08-27 17:17:27 +0100

    Fixed controller icons duplicating and sometimes not showing

--- Master/NucleusGaming/Coop/OpenXinputController.cs
+++ Master/NucleusGaming/Coop/OpenXinputController.cs
@@ -14,9 +14,73 @@ using SharpDX.XInput;
 
 namespace Nucleus.Gaming.Coop
 {
-	class OpenXinputController
+	public class OpenXinputController
 	{
-		static class Native
+		[StructLayout(LayoutKind.Sequential)]
+		public struct XINPUT_VIBRATION
+		{
+			public ushort wLeftMotorSpeed;
+			public ushort wRightMotorSpeed;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct XINPUT_GAMEPAD
+		{
+			public ushort wButtons;
+			public byte bLeftTrigger;
+			public byte bRightTrigger;
+			public short sThumbLX;
+			public short sThumbLY;
+			public short sThumbRX;
+			public short sThumbRY;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct XINPUT_STATE
+		{
+			public uint dwPacketNumber;
+			public XINPUT_GAMEPAD Gamepad;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct DeviceInfo_t
+		{
+			public uint status;
+			public IntPtr hDevice;
+			public IntPtr hGuideWait;
+			public byte dwUserIndex;
+			[MarshalAs(UnmanagedType.LPWStr)] public string lpDevicePath;
+			public uint dwDevicePathSize;
+			[MarshalAs(UnmanagedType.LPWStr)] public string field_18;
+			public uint field_1C;
+			public ushort wType;
+			public ushort field_22;
+			public XINPUT_STATE DeviceState;
+			public XINPUT_VIBRATION DeviceVibration;
+			public ushort field_38;
+			public ushort field_3A;
+			public ushort field_3C;
+			public ushort field_3E;
+			public ushort field_40;
+			public ushort field_42;
+			public ushort field_44;
+			public ushort field_46;
+			public ushort field_48;
+			public ushort field_4A;
+			public ushort field_4C;
+			public ushort vendorId;
+			public ushort productId;
+			public byte inputId;
+			public byte bTriggers;
+			public ushort wButtons;
+			public ushort LeftStickVirtualKey;
+			public ushort RightStickVirtualKey;
+			public ushort field_5A;
+			public ushort field_5C;
+			public ushort field_5E;
+		}
+
+		public static class Native
 		{
 			[DllImport("openxinput1_3.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "XInputGetKeystroke")]
 			public static extern int XInputGetKeystroke(int dwUserIndex, int dwReserved, out Keystroke keystrokeRef);
@@ -40,6 +104,13 @@ namespace Nucleus.Gaming.Coop
 
 			[DllImport("openxinput1_3.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "XInputGetCapabilities")]
 			public static extern int XInputGetCapabilities(int dwUserIndex, DeviceQueryType dwFlags, out Capabilities capabilitiesRef);
+
+			// [DllImport("openxinput1_3.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "GetDeviceOnPort")]
+			// public static extern int GetDeviceOnPort(uint dwUserIndex, ref DeviceInfo_t ppDevice, bool rescan);
+
+			[DllImport("openxinput1_3.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "GetDevicePath")]
+			[return: MarshalAs(UnmanagedType.BStr)]
+			public static extern string GetDevicePath(uint dwUserIndex);
 		}
 
 		private readonly int userIndex;
