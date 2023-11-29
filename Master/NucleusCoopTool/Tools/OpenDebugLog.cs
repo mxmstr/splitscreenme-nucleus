4542a7a1a4644bd449de4082b74c0ffb4fac725d
Mikou27 (nene27) <49500798+Mikou27@users.noreply.github.com>
2023-11-29 06:28:50 +0100

    -Added a prompt on crash so user can enable debug log(can be disabled by choosing "No" in the prompt window). -Added a debug log button (open debug-log.txt) if debug log is enabled. -Added Context.CopyFolder(string source, string destination). Copy a full folder from Gameplay{}. -Fixed a Null Object exception in GameProfile(screens array). -Remove 1 player restriction for splitdiv in GenericGameHandler. -Added a "hide desktop only" option for splitdiv(profile settings and settings) so we can hide desktop without resizing or offset the game windows.

---
+++ Master/NucleusCoopTool/Tools/OpenDebugLog.cs
@@ -0,0 +1,34 @@
+﻿using Nucleus.Gaming;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Nucleus.Coop.Tools
+{
+    internal class OpenDebugLog
+    {
+        public static void OpenDebugLogFile()
+        {
+            try
+            {
+                if (Globals.ini.IniReadValue("Dev", "TextEditorPath") != "Default")
+                {
+                    Process.Start(Application.StartupPath);
+                    Process.Start($"{Globals.ini.IniReadValue("Dev", "TextEditorPath")}", Path.Combine(Application.StartupPath, "debug-log.txt"));
+                }
+                else
+                {
+                    Process.Start(Application.StartupPath);
+                    Process.Start("notepad++.exe", Path.Combine(Application.StartupPath, "debug-log.txt"));
+                }
+
+            }
+            catch (Exception)
+            {
+                Process.Start(Application.StartupPath);
+                Process.Start("notepad.exe", Path.Combine(Application.StartupPath, "debug-log.txt"));
+            }
+        }
+    }
+}
